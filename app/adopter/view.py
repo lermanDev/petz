17a200f7fd15d54d14f6386d83eb60475429b4cd
Luis Medina <info@lerman.es>
2023-10-28 01:04:32 +0200

    [APP|MOD] Register, Log in and Log out

--- app/adopter/view.py
+++ app/adopter/view.py
@@ -1,17 +1,16 @@
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.shortcuts import render
 from .models import Adopter
 from .forms import AdopterRegistrationForm, UserLoginForm
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
 
 
 class AdopterCreateView(CreateView):
     model = Adopter
     form_class = AdopterRegistrationForm
     template_name = "registration/register.html"
-    success_url = reverse_lazy("login")
 
     def post(self, request):
         form = AdopterRegistrationForm(request.POST)
@@ -22,7 +21,15 @@ class AdopterCreateView(CreateView):
         )
         if form.is_valid():
             form.save()
-            return reverse_lazy("login")
+            return render(
+                request,
+                "registration/register.html",
+                {
+                    "form": form,
+                    "registered": True,
+                    "override_base_template": base_template,
+                },
+            )
 
         return render(
             request,
@@ -34,4 +41,33 @@ class AdopterCreateView(CreateView):
 class AdopterLoginView(LoginView):
     form_class = UserLoginForm
     template_name = "registration/login.html"
-    success_url = reverse_lazy("login")
+
+    # def post(self, request):
+    #     form = UserLoginForm(request, request.POST)
+    #     base_template = (
+    #         "partials/empty_base.html"
+    #         if request.htmx
+    #         else "registration/partials/base.html"
+    #     )
+    #     if form.is_valid():
+    #         user = authenticate(
+    #             username=form.cleaned_data["username"],
+    #             password=form.cleaned_data["password"],
+    #         )
+    #         if user is not None:
+    #             login(request, user)
+    #             return render(
+    #                 request,
+    #                 "registration/login.html",
+    #                 {
+    #                     "form": form,
+    #                     "override_base_template": base_template,
+    #                     "logged": True,
+    #                 },
+    #             )
+
+    #     return render(
+    #         request,
+    #         "registration/login.html",
+    #         {"form": form, "override_base_template": base_template},
+    #     )
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
 from adopter.view import AdopterCreateView, AdopterLoginView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", AdopterCreateView.as_view(), name="register"),
-    path("login/", AdopterLoginView.as_view(), name="login"),
+    path("login/", AdopterLoginView.as_view(next_page="login"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     # path("adopter/", include("django.contrib.auth.urls")),
 ]
 
