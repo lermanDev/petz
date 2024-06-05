8de187932a70a7e2241097f108461565b407326a
lermanDev <info@lerman.es>
2024-06-05 17:06:34 +0200

    [MOD] CSS and JS Optimization

--- app/theme/static/js/main.js
+++ app/theme/static/js/main.js
@@ -9,8 +9,8 @@ function preloader() {
 };
 
 $(window).on('load', function () {
-	preloader();
-	mainSlider();
+	//preloader();
+	//mainSlider();
 	wowAnimation();
 });
 
@@ -103,50 +103,50 @@ $(".search-close").on('click',function () {
 /*=============================================
 	=    		 Main Slider		      =
 =============================================*/
-function mainSlider() {
-	var BasicSlider = $('.slider-active');
-	BasicSlider.on('init', function (e, slick) {
-		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-		doAnimations($firstAnimatingElements);
-	});
-	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-		doAnimations($animatingElements);
-	});
-	// BasicSlider.slick({
-	// 	autoplay: false,
-	// 	autoplaySpeed: 10000,
-	// 	dots: false,
-	// 	fade: true,
-	// 	arrows: true,
-	// 	prevArrow: '<button type="button" class="slick-prev"><img src="/static/img/icon/arrow.png" /></button>',
-	// 	nextArrow: '<button type="button" class="slick-next"><img src="/static/img/icon/arrow.png" /></button>',
-	// 	responsive: [
-	// 		{ breakpoint: 767, settings: { dots: false, arrows: false } }
-	// 	]
-	// });
+// function mainSlider() {
+// 	var BasicSlider = $('.slider-active');
+// 	BasicSlider.on('init', function (e, slick) {
+// 		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+// 		doAnimations($firstAnimatingElements);
+// 	});
+// 	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+// 		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+// 		doAnimations($animatingElements);
+// 	});
+// 	// BasicSlider.slick({
+// 	// 	autoplay: false,
+// 	// 	autoplaySpeed: 10000,
+// 	// 	dots: false,
+// 	// 	fade: true,
+// 	// 	arrows: true,
+// 	// 	prevArrow: '<button type="button" class="slick-prev"><img src="/static/img/icon/arrow.png" /></button>',
+// 	// 	nextArrow: '<button type="button" class="slick-next"><img src="/static/img/icon/arrow.png" /></button>',
+// 	// 	responsive: [
+// 	// 		{ breakpoint: 767, settings: { dots: false, arrows: false } }
+// 	// 	]
+// 	// });
 
-	function doAnimations(elements) {
-		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		elements.each(function () {
-			var $this = $(this);
-			var $animationDelay = $this.data('delay');
-			var $animationDuration = $this.data('duration');
-			var $animationType = 'animated ' + $this.data('animation');
-			$this.css({
-				'animation-delay': $animationDelay,
-				'-webkit-animation-delay': $animationDelay,
-			});
-			$this.css({
-				'animation-duration': $animationDuration,
-				'-webkit-animation-duration': $animationDuration,
-			});
-			$this.addClass($animationType).one(animationEndEvents, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-}
+// 	function doAnimations(elements) {
+// 		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+// 		elements.each(function () {
+// 			var $this = $(this);
+// 			var $animationDelay = $this.data('delay');
+// 			var $animationDuration = $this.data('duration');
+// 			var $animationType = 'animated ' + $this.data('animation');
+// 			$this.css({
+// 				'animation-delay': $animationDelay,
+// 				'-webkit-animation-delay': $animationDelay,
+// 			});
+// 			$this.css({
+// 				'animation-duration': $animationDuration,
+// 				'-webkit-animation-duration': $animationDuration,
+// 			});
+// 			$this.addClass($animationType).one(animationEndEvents, function () {
+// 				$this.removeClass($animationType);
+// 			});
+// 		});
+// 	}
+// }
 
 
 /*=============================================
